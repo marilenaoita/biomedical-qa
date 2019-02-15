69d58d14ec0af31e8921c95f5647b730f2d17059
Marilena Oita <7187550+marilenaoita@users.noreply.github.com>
2019-02-15 13:28:42 +0100

    Update split_bioasq.py

--- biomedical_qa/tools/split_bioasq.py
+++ biomedical_qa/tools/split_bioasq.py
@@ -5,13 +5,13 @@ import json
 import tensorflow as tf
 import numpy as np
 
-tf.app.flags.DEFINE_string('bioasq_file', "/home/oitama1/workspace/biomedical-qa/data/bioasq/training7b.json", 'Path to the BioASQ JSON file.')
-tf.app.flags.DEFINE_string('out_dir', "/home/oitama1/workspace/biomedical-qa/data/bioasq/dev-train", 'Path to the output directory.')
+tf.app.flags.DEFINE_string('bioasq_file', os.environ['HOME']+"/workspace/biomedical-qa/data/bioasq/training7b.json", 'Path to the BioASQ JSON file.')
+tf.app.flags.DEFINE_string('out_dir', os.environ['HOME']+"/workspace/biomedical-qa/data/bioasq/dev-train", 'Path to the output directory.')
 tf.app.flags.DEFINE_string('dev_path_types', "factoid,list", 'Comma-separated list of question types.')
 tf.app.flags.DEFINE_string('random_assign_types', "yesno", 'Comma-separated list of question types.')
 tf.app.flags.DEFINE_integer('cross_validation_fold', 1, 'If greater then one, creates datasets for k-fold cross validation -> ignores random_assign_train_fractions & dev_id_file.')
 tf.app.flags.DEFINE_float('random_assign_train_fraction', 0.8, 'Fraction of train data.')
-tf.app.flags.DEFINE_string('dev_id_file', "/home/oitama1/workspace/biomedical-qa/data/bioasq/dev-train/dev.json", 'Path to a text file with dev question IDs, one ID per line.')
+tf.app.flags.DEFINE_string('dev_id_file', os.environ['HOME']+"/workspace/biomedical-qa/data/bioasq/dev-train/dev.json", 'Path to a text file with dev question IDs, one ID per line.')
 
 FLAGS = tf.app.flags.FLAGS
 
